5669bc68e49d986f452d081b61917351a9172607
jameswhinn <jameswhinn89@gmail.com>
2018-01-05 09:44:50 +0000

    Dynamic subnet (#6)
    * .
    
    * test
    
    * test
    
    * test
    
    * introduce functionality within module to calculate compulsary sbnets from source range

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.name}-vnet-${var.env}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  address_space       = "${var.address_space}"
+  address_space       = ["${var.address_space}"]
   location            = "${azurerm_resource_group.rg.location}"
 }
 
 resource "azurerm_subnet" "sb" {
-  count                = "${length(var.address_prefixes)}"
+  count                = "4"
   name                 = "${var.name}-subnet-${count.index}-${var.env}"
   resource_group_name  = "${azurerm_virtual_network.vnet.resource_group_name}"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  address_prefix       = "${element(var.address_prefixes,count.index)}"
+  address_prefix       = "${cidrsubnet("${var.source_range}", 7, count.index)}"
 }
--- variables.tf
+++ variables.tf
@@ -11,11 +11,11 @@ variable "address_space" {
   default = ["10.0.0.0/16"]
 }
 
-variable "address_prefixes" {
-  default = ["10.0.0.0/24"]
-}
-
 variable "env" {
   type    = "string"
   default = "local"
+}
+
+variable "source_range" {
+  type = "string"
 }
