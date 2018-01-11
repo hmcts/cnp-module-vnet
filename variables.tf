e8e41ea1f3d85f10b4e4a8bc4281ac4e68a1a776
Calin Marina <calin.marina@gmail.com>
2018-01-11 14:26:03 +0000

    add dns_servers to vnet

--- variables.tf
+++ variables.tf
@@ -7,15 +7,20 @@ variable "location" {
   default = "South UK"
 }
 
-variable "address_space" {
-  default = ["10.0.0.0/16"]
-}
+variable "address_space" {}
 
 variable "env" {
   type    = "string"
   default = "local"
 }
 
+variable "lb_private_ip_address" {}
+
 variable "source_range" {
   type = "string"
+}
+
+variable "microsoft_external_dns" {
+  default     = ["168.63.129.16", "172.16.0.10"]
+  description = "List of external DNS servers, default currently including tactical dns."
 }
