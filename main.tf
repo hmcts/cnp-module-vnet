e5b6eaddc1092ad84c46c570cbd00b50f9d513b4
James Whinn <jameswhinn89@gmail.com>
2018-03-23 16:29:10 +0000

    debug

--- main.tf
+++ main.tf
@@ -10,29 +10,26 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "${azurerm_resource_group.rg.location}"
 }
 
-resource "azurerm_subnet" "sb" {
-  count                = "4"
-  name                 = "${var.name}-subnet-${count.index}-${var.env}"
-  resource_group_name  = "${azurerm_virtual_network.vnet.resource_group_name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  address_prefix       = "${cidrsubnet("${var.source_range}", 4, count.index)}"
+# resource "azurerm_subnet" "sb" {
+#   count                = "4"
+#   name                 = "${var.name}-subnet-${count.index}-${var.env}"
+#   resource_group_name  = "${azurerm_virtual_network.vnet.resource_group_name}"
+#   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+#   address_prefix       = "${cidrsubnet("${var.source_range}", 4, count.index)}"
+# }
+
+data "template_file" "privatedns" {
+  template = "${file("${path.module}/template/azuredns.json")}"
 }
 
-# data "template_file" "privatedns" {
-#   template = "${file("${path.module}/template/azuredns.json")}"
-# }
+resource "azurerm_template_deployment" "azuredns" {
+  template_body       = "${data.template_file.privatedns.rendered}"
+  name                = "azure-dns-${var.env}"
+  deployment_mode     = "Incremental"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
 
-
-# resource "azurerm_template_deployment" "azuredns" {
-#   template_body       = "${data.template_file.privatedns.rendered}"
-#   name                = "azure-dns-${var.env}"
-#   deployment_mode     = "Incremental"
-#   resource_group_name = "${azurerm_resource_group.rg.name}"
-
-
-#   parameters = {
-#     zoneName = "core-compute-${var.env}.internal"
-#     vnetName = "${azurerm_virtual_network.vnet.name}"
-#   }
-# }
-
+  parameters = {
+    zoneName = "core-compute-${var.env}.internal"
+    vnetName = "${azurerm_virtual_network.vnet.name}"
+  }
+}
