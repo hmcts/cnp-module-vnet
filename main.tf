39435d3db65e7cdc17d40c3ca0784e2e46134d1e
Hannah Smith <hannahs@kainos.com>
2020-08-13 18:50:00 +0100

    update address prefix to list

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_subnet" "sb" {
   name                 = "${var.name}-subnet-${count.index}-${var.env}"
   resource_group_name  = "${azurerm_virtual_network.vnet.resource_group_name}"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  address_prefixes       = "${cidrsubnet("${var.source_range}", 4, count.index)}"
+  address_prefixes     = ["${cidrsubnet("${var.source_range}", 4, count.index)}"]
 
   lifecycle {
     ignore_changes = [address_prefixes]
